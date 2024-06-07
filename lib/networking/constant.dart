c08ea8dc9e3aeb7947cd1248ffae671c4f60cce6
vaishnav <vvaishnav749@gmail.com>
2024-06-07 19:02:48 +0530

    addmember

--- lib/networking/constant.dart
+++ lib/networking/constant.dart
@@ -1,3 +1,3 @@
 // const String baseURL = "https://admin.marketjourney.in";
 // const String baseURL = "http://192.168.29.77:6003";
-const String baseURL = "http://192.168.74.18:6003";
+const String baseURL = "http://172.20.10.2:6003";
