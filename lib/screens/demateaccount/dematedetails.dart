6f7d901d1694e25606b4182b9051f6934f9f6ece
Amal Dominic <107304411+amaldominic816@users.noreply.github.com>
2024-06-19 22:24:51 +0530

    Logo Updated

--- lib/screens/demateaccount/dematedetails.dart
+++ lib/screens/demateaccount/dematedetails.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:master_journey/screens/demateaccount/widget/demataccount.dart';
 
 import '../../services/bank_service.dart';
+import '../../services/profile_service.dart';
 import '../../support/logger.dart';
 
 class Dematedetails extends StatefulWidget {
@@ -16,11 +17,28 @@ class Dematedetails extends StatefulWidget {
 class _DematedetailsState extends State<Dematedetails> {
   var userid;
   List<dynamic> demateAccounts = [];
+  Map<String, dynamic>? profiledata;
 
   @override
   void initState() {
     super.initState();
     _Dematedetail();
+    _profileData();
+  }
+
+  Future<void> _profileData() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      userid = prefs.getString('userid');
+      var response = await ProfileService.profile();
+      log.i('Profile data show.... $response');
+      setState(() {
+        profiledata = response;
+      });
+    } catch (e) {
+      log.e('Error fetching profile data: $e');
+      // Handle error appropriately, e.g., show a snackbar or a message to the user
+    }
   }
 
   Future<void> _Dematedetail() async {
@@ -49,7 +67,48 @@ class _DematedetailsState extends State<Dematedetails> {
         backgroundColor: marketbg,
       ),
       backgroundColor: marketbg,
-      body: ListView.builder(
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: bluem,
+        ),
+        backgroundColor: yellow,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Demataccount()),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    if (profiledata == null) {
+      return Center(child: CircularProgressIndicator());
+    } else if (profiledata!['packageType'] == 'Franchise') {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logo/exclamation-mark.png', // Replace with your image path
+              height: 25,
+              width: 25,
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                'You Are Not Authorized to View This Page',
+                style: TextStyle(color: black, fontSize: 12),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return ListView.builder(
         itemCount: demateAccounts.length,
         padding: EdgeInsets.symmetric(vertical: 10),
         itemBuilder: (BuildContext context, int index) {
@@ -254,20 +313,7 @@ class _DematedetailsState extends State<Dematedetails> {
             ),
           );
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.add,
-          color: bluem,
-        ),
-        backgroundColor: yellow,
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const Demataccount()),
-          );
-        },
-      ),
-    );
+      );
+    }
   }
 }
